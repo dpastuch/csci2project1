f849501e14483cd60c7d7b57887e7e87e8b73ab7
dpastuch <d1pastuch@rochester.rr.com>
2017-03-11 18:23:16 -0500

    Continued work on equationProcessor.

--- src/Processors/EquationProcessor.java
+++ src/Processors/EquationProcessor.java
@@ -1,6 +1,5 @@
 package Processors;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import Util.SymbolTable;
 
@@ -36,10 +35,17 @@ public class EquationProcessor {
 
     private void processEquation(String eq) {
         String[] tokens = eq.split(" ");
-        ArrayList<String> tokenList = new ArrayList(Arrays.asList(tokens));
-        if(tokenList.get(1) == "=") {
-            assignVar(tokenList.get(0), evaluate(tokenList));
+        ArrayList<String> tokenList = new ArrayList<>(Arrays.asList(tokens));
+        if(tokenList.get(0).equals("while(")) {
+            whileLoop(eq.substring(6));
         }
+        else if(tokenList.get(0).equals("if(")) {
+            ifStatement(eq.substring(3));
+        }
+        else {
+            evalStatement(eq);
+        }
+
     }
 
     private void assignVar(String var, int val) {
@@ -50,28 +56,65 @@ public class EquationProcessor {
         symbolTable.dump();
     }
 
-    private void print(ArrayList<String> tokenList) {
-        System.out.println(evaluate(tokenList));
+    private void print(String tokenString) {
+        String tokens = tokenString.trim();
+        String[] tokenList = tokens.split(" ");
+        ArrayList<String> tokenList2 = new ArrayList<>(Arrays.asList(tokenList));
+        System.out.println(evaluate(tokenList2));
     }
 
-    private void ifStatement() {
-
+    private void ifStatement(String tokenString) {
+        String tokens = tokenString.trim();
+        String[] tokenList = tokens.split(",");
+        ArrayList<String> tokenList2 = new ArrayList<>(Arrays.asList(tokenList));
+        if(evalStatement(tokenList2.get(0)) == 1) {
+            evalStatement(tokenList2.get(1));
+        }
+        else {
+            evalStatement(tokenList2.get(2));
+        }
     }
 
-    private void whileLoop(String tokens) {
+    private void whileLoop(String tokenString) {
+        String tokens = tokenString.trim();
         String[] tokenList = tokens.split(";");
-        ArrayList<String> tokenList2 = new ArrayList(Arrays.asList(tokens));
-        String[] tokens2 = tokenList2.get(0).split(" ");
-        ArrayList<String> tokenList3 = new ArrayList(Arrays.asList(tokens2));
-        while(evaluate(tokenList3) == 1) {
-            for(String t : tokenList3) {
-
+        ArrayList<String> tokenList2 = new ArrayList<>(Arrays.asList(tokenList));
+        while(evalStatement(tokenString) == 1) {
+            for(String t : tokenList2) {
+                evalStatement(t);
             }
         }
+    }
+
+    private int evaluate(String tokens) {
+        String[] tokenArray = tokens.split(" ");
+        ArrayList<String> tokenList = new ArrayList<>(Arrays.asList(tokenArray));
+        processor.constructTree(tokenList);
+        return processor.evaluateTree(symbolTable);
     }
 
     private int evaluate(ArrayList<String> tokenList) {
         processor.constructTree(tokenList);
         return processor.evaluateTree(symbolTable);
+    }
+
+    private int evalStatement(String tokens) {
+        String[] tokenArray = tokens.split(" ");
+        ArrayList<String> tokenList = new ArrayList<>(Arrays.asList(tokenArray));
+            if (tokenList.get(0).equals("print(")) {
+                print(tokens.substring(6));
+            }
+            else if (tokenList.get(0).equals("printVars()")) {
+                printVars();
+            }
+            else if(tokenList.size() > 2) {
+                if (tokenList.get(1).equals("=")) {
+                    assignVar(tokenList.get(0), evaluate(tokenList.get(2)));
+                }
+            }
+            else {
+                return evaluate(tokenList);
+            }
+            return 0;
     }
 }
